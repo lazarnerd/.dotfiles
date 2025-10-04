e7e242b4c4bbde988b7033884084a16bba179794
Daniel Neururer <danielxiv64@gmail.com>
2025-10-04 16:40:30 +0200

    updated installer script

--- scripts/install_nixos.sh
+++ scripts/install_nixos.sh
@@ -1,7 +1,6 @@
 ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/.."
 NIX_DIR="$ROOT_DIR/nix"
-nix --experimental-features "nix-command flakes" run github:nix-community/disko/latest -- --mode destroy,format,mount $NIX_DIR/hosts/acer/disko.nix
-nixos-install --flake "$NIX_DIR/flake.nix#acer"
+nix --experimental-features "nix-command flakes" run github:nix-community/disko/latest -- --mode destroy,format,mount --yes-wipe-all-disks $NIX_DIR/hosts/acer/disko.nix
 
 cp -R $ROOT_DIR /mnt/home/nerd/.dotfiles
 cp -R $ROOT_DIR/.ssh/* /mnt/home/nerd/.ssh
@@ -9,3 +8,5 @@ cp -R $ROOT_DIR/.ssh/* /mnt/home/nerd/.ssh
 chown -R 1000:100 /mnt/home/nerd
 chmod 700 /mnt/home/nerd/.ssh
 chmod 600 /mnt/home/nerd/.ssh/*
+
+nixos-install --flake "$NIX_DIR/flake.nix#acer"
